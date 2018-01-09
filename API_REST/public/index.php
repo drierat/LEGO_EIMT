e590ee6a36b533c407780773398cf6131fa978a2
afalguerasv <adriafalgueras@gmail.com>
2018-01-09 18:39:48 +0100

    añadidos cambios para insertar en base de datos
    Todavía no funciona, sigo buscando el error en la inserción en la base de datos

--- API_REST/public/index.php
+++ API_REST/public/index.php
@@ -54,12 +54,12 @@ $app->post('/usuari/login', function (Request $request, Response $response){
 		$password = $allPostPutParams['pass'];
 
 		include ("connexio.php"); // Include the file with the connection functions to the DB
-	
+
 		$bd = connectarBD();
 		$sqlUserName = "SELECT idPRA,Nom,Cognoms,Rol FROM pra WHERE idPRA = '$user' and idPRA = '$password'"; // CHANGE!!! quan el password sigui el definitiu
 		$comprobaUser = mysqli_query ($bd, $sqlUserName);
 		$usuari = mysqli_fetch_object($comprobaUser);
-	
+
 		// Preparació dades UTF8 per generar jSON
 		$NomUTF8 = utf8_encode($usuari->Nom);
 		$CognomsUTF8 = utf8_encode($usuari->Cognoms);
@@ -68,9 +68,9 @@ $app->post('/usuari/login', function (Request $request, Response $response){
 
 		$response = $response->withJson($retorn, 200); // OK
 		return($response);
-	  	} 
+	  	}
 	  	catch (Exception $e) {
-    	// Assign the error message and use it to handle any customer messages or logging 
+    	// Assign the error message and use it to handle any customer messages or logging
     		$error = $e->getMessage();
 
   		}
@@ -84,9 +84,9 @@ $app->post('/tipusDePesaTots', function (Request $request, Response $response){
 		// No input params
 		//$allPostPutParams = $request->getParsedBody();
 		//$user = $allPostPutParams['nom'];
-		
+
 		include ("connexio.php"); // Include the file with the connection functions to the DB
-	
+
 		$bd = connectarBD();
 		$sqlTipusPesa = "SELECT idTipus, idCategoria FROM tipuspesa";
 		$tipusDePesaTots = mysqli_query ($bd, $sqlTipusPesa);
@@ -103,7 +103,7 @@ $app->post('/tipusDePesaTots', function (Request $request, Response $response){
 
 
 //		$llistatTipusPesa = mysqli_fetch_object($tipusDePesaTots);
-	
+
 		// Preparació dades UTF8 per generar jSON
 /*		$tipus1 = $llistatTipusPesa[1];
 
@@ -116,7 +116,7 @@ $app->post('/tipusDePesaTots', function (Request $request, Response $response){
 
 
 
-//$allRows = array(); 
+//$allRows = array();
 //while($row = mysql_fetch_array($result)) {$allRows[] = $row }
 
 //$retorn=[];
@@ -126,9 +126,9 @@ $app->post('/tipusDePesaTots', function (Request $request, Response $response){
 
 		$response = $response->withJson($retorn, 200); // OK
 		return($response);
-	  	} 
+	  	}
 	  	catch (Exception $e) {
-    	// Assign the error message and use it to handle any customer messages or logging 
+    	// Assign the error message and use it to handle any customer messages or logging
     		$error = $e->getMessage();
   		}
 });
@@ -143,7 +143,7 @@ $app->post('/usuari/llistatPeces', function (Request $request, Response $respons
 		$any = $allPostPutParams['any'];
 
 		include ("connexio.php"); // Crida al fitxer de la connexió
-	
+
 		$bd = connectarBD();
 		//$retorn = array();
 		for ($year = $any-1; $year<=$any+1;$year++){ //CHANGE to a single query!
@@ -166,9 +166,9 @@ $app->post('/usuari/llistatPeces', function (Request $request, Response $respons
 
 		$response = $response->withJson($retorn, 200); // OK
 		return($response);
-	  	} 
+	  	}
 	  	catch (Exception $e) {
-    	// Assign the error message and use it to handle any customer messages or logging 
+    	// Assign the error message and use it to handle any customer messages or logging
     		$error = $e->getMessage();
 
   		}
@@ -190,20 +190,20 @@ $app->post('/usuari/pesa', function (Request $request, Response $response){
 		$sqlItem = "SELECT * FROM obj_any_pra WHERE idPRA = '$user' and any = '$any'";
 		$queryPecesAnyPRA = mysqli_query ($bd, $sqlItem);
 		$PecesAnyPRA = mysqli_fetch_object($queryPecesAnyPRA);
-		
+
 		$id = $PecesAnyPRA->$columna; // Aquest és el codi (únic) de la peça
 		$sqlItem2 = "SELECT * FROM pesa WHERE idPesa = '$id'";
 		$queryPesa = mysqli_query ($bd, $sqlItem2);
 		$Pesa = mysqli_fetch_object($queryPesa);
-		
+
 		$id = $Pesa->idPesa;
 		$etiqPesaUTF8 = utf8_encode($Pesa->etiqueta);
 		$tipusPesaUTF8 = utf8_encode($Pesa->idTipus);
-		
+
 			$sqlItem3 = "SELECT explicacio FROM tipuspesa WHERE idTipus = '$tipusPesaUTF8'";
 			$queryTipusConcat = mysqli_query ($bd, $sqlItem3);
 			$tipusConcat = mysqli_fetch_object($queryTipusConcat);
-	
+
 		$tipusConcatUTF8 = utf8_encode($tipusConcat->explicacio);
 		$aplicatPesaUTF8 = utf8_encode($Pesa->aplicat);
 		$comprPesaUTF8 = utf8_encode($Pesa->compromis);
@@ -212,13 +212,13 @@ $app->post('/usuari/pesa', function (Request $request, Response $response){
 		$validtPesaUTF8 = utf8_encode($Pesa->validat);
 		$comenPesaUTF8 = utf8_encode($Pesa->comentaris);
 
-		$dadesPesa = array ("id" => $id,"etiqueta" => $etiqPesaUTF8, "tipus" => $tipusPesaUTF8, "tipusConcat" => $tipusConcatUTF8, "aplicat" => $aplicatPesaUTF8, "compromis" => $comprPesaUTF8, "acompliment" => $acompPesaUTF8, "validacio" => $validcPesaUTF8, "validat" => $validtPesaUTF8, "comentaris" => $comenPesaUTF8);		
+		$dadesPesa = array ("id" => $id,"etiqueta" => $etiqPesaUTF8, "tipus" => $tipusPesaUTF8, "tipusConcat" => $tipusConcatUTF8, "aplicat" => $aplicatPesaUTF8, "compromis" => $comprPesaUTF8, "acompliment" => $acompPesaUTF8, "validacio" => $validcPesaUTF8, "validat" => $validtPesaUTF8, "comentaris" => $comenPesaUTF8);
 		$retorn = $dadesPesa;
 		$response = $response->withJson($retorn, 200);
 		return($response);
-	  	} 
+	  	}
 	  	catch (Exception $e) {
-    	// Assign the error message and use it to handle any customer messages or logging 
+    	// Assign the error message and use it to handle any customer messages or logging
     		$error = $e->getMessage();
   		}
 });
@@ -226,41 +226,60 @@ $app->post('/usuari/pesa', function (Request $request, Response $response){
 $app->post('/usuari/altaPeces', function (Request $request, Response $response){
 
 	try{
-		$allPostPutParams = $request->getParsedBody();
-		$usuari = $allPostPutParams['nom'];
-		$any = $allPostPutParams['any'];
-		$posicio = $allPostPutParams['posicio'];
-		$etiqueta = $allPostPutParams['etiqueta'];
-		$tipus = $allPostPutParams['tipus'];
-		$aplicat = $allPostPutParams['aplicat'];
-		$compromis = $allPostPutParams['compromis'];
-		$acompliment = $allPostPutParams['acompliment'];
-		$validacio = $allPostPutParams['validacio'];
-		$validat = $allPostPutParams['validat'];
-		$comentaris = $allPostPutParams['comentaris'];
+		//$allPostPutParams = $request->getParsedBody(); $user_name  = $_POST['user_name'];
+		$usuari = $_POST['nom'];
+		$any = $_POST['any'];
+		$posicio = $_POST['posicio'];
+		$etiqueta = $_POST['etiqueta'];
+		$tipus = $_POST['tipus'];
+		$aplicat = $_POST['aplicat'];
+		$compromis = $_POST['compromis'];
+		$acompliment = $_POST['acompliment'];
+		//$validacio = $allPostPutParams['validacio'];
+		//$validat = $allPostPutParams['validat'];
+		$comentaris = $_POST['comentaris'];
 
-		include ("connexio.php"); 
+		include ("connexio.php");
 		$bd = connectarBD();
-		
+
+		$consulta="SELECT * FROM obj_any_pra WHERE idPRA = '$usuari' and any = '$any'";
+		$qry=mysql_query($bd, $consulta) or die (mysql_error());
+		$resultado = mysqli_fetch_object($qry);
+		if (mysql_num_rows($resultado)>0)
+		{
+			//si ja existeix l'objecte a la taula fem update
+			$print = print('ja existeix');
+			$columna = "idPesa".$posicio;
+			$sqlItem2 = "UPDATE obj_any_pra SET '$columna'= 'Exemple' WHERE idPRA = '$usuari' and any = '$any'";
+			$queryAltaPesaPra = mysqli_query ($bd, $sqlItem2);
+			$altaPesaPra = mysqli_fetch_object($queryAltaPesaPra);
+		} else {
+			  //no existeix objecte per tant fem un insert
+			$print = print('no existeix');
+			$columna = "idPesa".$posicio;
+			$sqlItem2 = "INSERT INTO obj_any_pra ('idPRA', 'any', 'idPesa'.$posicio, 'retorn', 'estat') VALUES ('{$usuari}', '{$any}', '{NULL}','{NULL}','{NULL}')";
+			$queryAltaPesaPra = mysqli_query($bd, $sqlItem2);
+			$altaPresaPra = mysqli_fetch_object($queryAltaPesaPra);
+		}
+
+		/*//COM OBTINC EL ID DE LA NOVA PEÇA CREADA??
+		//SELECCIONEM COLUMNA I INSERIM PEÇA A PRA
+		$columna = "idPesa".$posicio;
+		$sqlItem2 = "UPDATE obj_any_pra SET '$columna'= 'Exemple' WHERE idPRA = '$usuari' and any = '$any'";
+		$queryAltaPesaPra = mysqli_query ($bd, $sqlItem2);
+		$altaPesaPra = mysqli_fetch_object($queryAltaPesaPra);*/
+
 			//INSERIM PEÇA
-		$sqlItem = "INSERT INTO pesa ('idPesa', 'idTipus', 'etiqueta', 'aplicat', 'compromis', 'acompliment', 'validacio', 'validat', 'comentaris', 'idPesa_modificada') VALUES (NULL, '$tipus', '$etiqueta', '$aplicat', '$compromis', '$acompliment', '$validacio', '$validat', '$comentaris', NULL)";
+		$sqlItem = "INSERT INTO pesa ('idPesa', 'idTipus', 'etiqueta', 'aplicat', 'compromis', 'acompliment', 'validacio', 'validat', 'comentaris', 'idPesa_modificada') VALUES ('{NULL}', '{$tipus}', '{$etiqueta}', '{$aplicat}', '{$compromis}', '{$acompliment}', '{NULL}', '{0}' '{$comentaris}', '{NULL}')";
 		$queryAltaPesa = mysqli_query ($bd, $sqlItem);
 		$altaPesa = mysqli_fetch_object($queryAltaPesa);
-			
-	/* COM OBTINC EL ID DE LA NOVA PEÇA CREADA??  
-			//SELECCIONEM COLUMNA I INSERIM PEÇA A PRA
-		$columna = "idPesa". $posicio;
-		$sqlItem2 = "UPDATE obj_any_pra SET '$columna'= ?? WHERE idPRA = '$usuari' and any = '$any'";
-		$queryAltaPesaPra = mysqli_query ($bd, $sqlItem2);
-		$altaPesaPra = mysqli_fetch_object($queryAltaPesaPra);
-	*/
-		
-		$retorn = $etiqueta;
+		echo $print;
+		$retorn = $print;
 		$response = $response->withJson($retorn, 200);
 		return($response);
-	  	} 
+	  	}
 	  	catch (Exception $e) {
-    	// Assign the error message and use it to handle any customer messages or logging 
+    	// Assign the error message and use it to handle any customer messages or logging
     		$error = $e->getMessage();
   		}
 });
@@ -286,16 +305,16 @@ $app->post('/usuari/modificaPesa', function (Request $request, Response $respons
 		$sqlItemA = "SELECT * FROM pesa WHERE idPesa = '$idPesa'";
 		$queryPesaA = mysqli_query ($bd, $sqlItemA);
 		$PesaA = mysqli_fetch_object($queryPesaA);
-		
-		
+
+
 		// FER UN INSERT AMB id NOU (AUTO) I AMB NºPEÇA ANTIGA A id_pesaModificada
-		
-		
+
+
 		$response = $response->withJson($retorn, 200);
 		return($response);
-	  	} 
+	  	}
 	  	catch (Exception $e) {
-    	// Assign the error message and use it to handle any customer messages or logging 
+    	// Assign the error message and use it to handle any customer messages or logging
     		$error = $e->getMessage();
   		}
 });
