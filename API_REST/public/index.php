6b6e8037045776786e8c8ea17f4de2112e4d1683
Daniel Riera i Terrén <daniel.riera@gmail.com>
2018-04-10 16:57:12 +0200

    Create new piece
    Now it is possible to create a new piece. It is stored in the DB

--- API_REST/public/index.php
+++ API_REST/public/index.php
@@ -9,50 +9,34 @@ if (PHP_SAPI == 'cli-server') {
         return false;
     }
 }
-
 require __DIR__ . '/../vendor/autoload.php';
-
 session_start();
-
 // Instantiate the app
 $settings = require __DIR__ . '/../src/settings.php';
 $app = new \Slim\App($settings);
-
 // Set up dependencies
 require __DIR__ . '/../src/dependencies.php';
-
 // Register middleware
 require __DIR__ . '/../src/middleware.php';
-
 // Register routes
 require __DIR__ . '/../src/routes.php';
-
 // Run app
 $app->run();
-
 */
-
 header("Content-Type: text/html;charset=utf-8");
-
 use \Psr\Http\Message\ServerRequestInterface as Request;
 use \Psr\Http\Message\ResponseInterface as Response;
-
 require '../vendor/autoload.php';
-
 // Configuració
 $config['displayErrorDetails'] = true;
 $config['addContentLengthHeader'] = false;
-
 $app = new \Slim\App(["settings" => $config]);
-
 // /usuari/login (POST) retorna, donats userId i password, el nom, cognoms i rol del PRA
 $app->post('/usuari/login', function (Request $request, Response $response){
-
 	try{
 		$allPostPutParams = $request->getParsedBody();
 		$user = $allPostPutParams['nom'];
 		$password = $allPostPutParams['pass'];
-
 		include ("connexio.php"); // Include the file with the connection functions to the DB
 	
 		$bd = connectarBD();
@@ -63,67 +47,29 @@ $app->post('/usuari/login', function (Request $request, Response $response){
 		// Preparació dades UTF8 per generar jSON
 		$NomUTF8 = utf8_encode($usuari->Nom);
 		$CognomsUTF8 = utf8_encode($usuari->Cognoms);
-
 		$retorn = array("nom" => $NomUTF8, "cognoms" => $CognomsUTF8, "rol" => $usuari->Rol);
-
 		$response = $response->withJson($retorn, 200); // OK
 		return($response);
 	  	} 
 	  	catch (Exception $e) {
     	// Assign the error message and use it to handle any customer messages or logging 
     		$error = $e->getMessage();
-
   		}
 });
 
-
 // /tipusDePesaTots (POST) retorna tots els tipus de peça introduïts al sistema
 $app->post('/tipusDePesaTots', function (Request $request, Response $response){
-
 	try{
-		// No input params
-		//$allPostPutParams = $request->getParsedBody();
-		//$user = $allPostPutParams['nom'];
-		
 		include ("connexio.php"); // Include the file with the connection functions to the DB
 	
 		$bd = connectarBD();
 		$sqlTipusPesa = "SELECT idTipus, idCategoria FROM tipuspesa";
 		$tipusDePesaTots = mysqli_query ($bd, $sqlTipusPesa);
-
 		$tipus = mysqli_fetch_all ($tipusDePesaTots, MYSQLI_ASSOC);
 		$registres = count($tipus);
-
-
 		for ($i=0; $i<$registres; $i++){
-			//$dadesTipus = array ("idTipus" => $tipus[$i]->"idTipus", "idCategoria" => $tipus[$i]->"idCategoria");
 			$retorn[$i] = $tipus[$i];
-			//$retorn[$i] = $dadesTipus;
 		}
-
-
-//		$llistatTipusPesa = mysqli_fetch_object($tipusDePesaTots);
-	
-		// Preparació dades UTF8 per generar jSON
-/*		$tipus1 = $llistatTipusPesa[1];
-
-		$NomUTF8 = utf8_encode($tipus1->idTipus);
-		$CognomsUTF8 = utf8_encode($tipus1->idCategoria);
-		$RolUTF8 = utf8_encode($tipus1->explicacio);
-
-		$retorn = array("Tipus" => $NomUTF8, "Categoria" => $CognomsUTF8, "explicacio" => $usuari->Rol);
-*/
-
-
-
-//$allRows = array(); 
-//while($row = mysql_fetch_array($result)) {$allRows[] = $row }
-
-//$retorn=[];
-//$retorn = $llistatTipusPesa->idTipus;
-//$retorn = array("a" => 10, "b" => 20);
-
-
 		$response = $response->withJson($retorn, 200); // OK
 		return($response);
 	  	} 
@@ -133,15 +79,12 @@ $app->post('/tipusDePesaTots', function (Request $request, Response $response){
   		}
 });
 
-
 //usuari/llistatPeces (POST) retorna, donat userId i any, els idPesa de les 10 peces d'aquell PRA en l'any indicat
 $app->post('/usuari/llistatPeces', function (Request $request, Response $response){
-
 	try{
 		$allPostPutParams = $request->getParsedBody();
 		$user = $allPostPutParams['nom'];
 		$any = $allPostPutParams['any'];
-
 		include ("connexio.php"); // Crida al fitxer de la connexió
 	
 		$bd = connectarBD();
@@ -163,29 +106,23 @@ $app->post('/usuari/llistatPeces', function (Request $request, Response $respons
 				$retorn[$indexPesaActual] = $dadesPesa;
 			}
 		}
-
 		$response = $response->withJson($retorn, 200); // OK
 		return($response);
 	  	} 
 	  	catch (Exception $e) {
     	// Assign the error message and use it to handle any customer messages or logging 
     		$error = $e->getMessage();
-
   		}
 });
-
 //usuari/Peça (POST) retorna, donat userId i any, la peça clicada d'aquell PRA en l'any indicat
 $app->post('/usuari/pesa', function (Request $request, Response $response){
-
 	try{
 		$allPostPutParams = $request->getParsedBody();
 		$user = $allPostPutParams['nom'];
 		$any = $allPostPutParams['any'];
 		$pos = $allPostPutParams['posicio'];
-
 		include ("connexio.php"); // Crida al fitxer de la connexió
 		$bd = connectarBD();
-
 		$columna = "idPesa". $pos; // Vull el codi de la idPesaX
 		$sqlItem = "SELECT * FROM obj_any_pra WHERE idPRA = '$user' and any = '$any'";
 		$queryPecesAnyPRA = mysqli_query ($bd, $sqlItem);
@@ -211,7 +148,6 @@ $app->post('/usuari/pesa', function (Request $request, Response $response){
 		$validcPesaUTF8 = utf8_encode($Pesa->validacio);
 		$validtPesaUTF8 = utf8_encode($Pesa->validat);
 		$comenPesaUTF8 = utf8_encode($Pesa->comentaris);
-
 		$dadesPesa = array ("id" => $id,"etiqueta" => $etiqPesaUTF8, "tipus" => $tipusPesaUTF8, "tipusConcat" => $tipusConcatUTF8, "aplicat" => $aplicatPesaUTF8, "compromis" => $comprPesaUTF8, "acompliment" => $acompPesaUTF8, "validacio" => $validcPesaUTF8, "validat" => $validtPesaUTF8, "comentaris" => $comenPesaUTF8);		
 		$retorn = $dadesPesa;
 		$response = $response->withJson($retorn, 200);
@@ -222,40 +158,49 @@ $app->post('/usuari/pesa', function (Request $request, Response $response){
     		$error = $e->getMessage();
   		}
 });
-
-$app->post('/usuari/altaPeces', function (Request $request, Response $response){
-
+//usuari/Peça (POST) retorna, donat userId i any, la peça clicada d'aquell PRA en l'any indicat
+$app->post('/usuari/altaPesa', function (Request $request, Response $response){
 	try{
 		$allPostPutParams = $request->getParsedBody();
-		$usuari = $allPostPutParams['nom'];
-		$any = $allPostPutParams['any'];
-		$posicio = $allPostPutParams['posicio'];
+		$user = $allPostPutParams['user'];
+		$year = $allPostPutParams['any'];
+		$position = $allPostPutParams['posicio'];
 		$etiqueta = $allPostPutParams['etiqueta'];
-		$tipus = $allPostPutParams['tipus'];
+		$tipusP = $allPostPutParams['tipusP'];
 		$aplicat = $allPostPutParams['aplicat'];
 		$compromis = $allPostPutParams['compromis'];
 		$acompliment = $allPostPutParams['acompliment'];
-		$validacio = $allPostPutParams['validacio'];
-		$validat = $allPostPutParams['validat'];
+//		$validacio = $allPostPutParams['validacio'];
+//		$validat = $allPostPutParams['validat'];
 		$comentaris = $allPostPutParams['comentaris'];
-
 		include ("connexio.php"); 
 		$bd = connectarBD();
 		
-			//INSERIM PEÇA
-		$sqlItem = "INSERT INTO pesa ('idPesa', 'idTipus', 'etiqueta', 'aplicat', 'compromis', 'acompliment', 'validacio', 'validat', 'comentaris', 'idPesa_modificada') VALUES (NULL, '$tipus', '$etiqueta', '$aplicat', '$compromis', '$acompliment', '$validacio', '$validat', '$comentaris', NULL)";
+		// Tot això s'hauria de fer de manera íntegra. Si no es completa, s'hauria de fer un rollback
+
+		//INSERT NEW PIECE IN pesa TABLE
+		$sqlItem = "INSERT INTO pesa (idTipus, etiqueta, aplicat, compromis, acompliment, validacio, validat, comentaris, idPesa_modificada) VALUES ('$tipusP', '$etiqueta', '$aplicat', '$compromis', '$acompliment', 'PRA' , '0' , '$comentaris', NULL);";
 		$queryAltaPesa = mysqli_query ($bd, $sqlItem);
-		$altaPesa = mysqli_fetch_object($queryAltaPesa);
-			
-	/* COM OBTINC EL ID DE LA NOVA PEÇA CREADA??  
-			//SELECCIONEM COLUMNA I INSERIM PEÇA A PRA
-		$columna = "idPesa". $posicio;
-		$sqlItem2 = "UPDATE obj_any_pra SET '$columna'= ?? WHERE idPRA = '$usuari' and any = '$any'";
-		$queryAltaPesaPra = mysqli_query ($bd, $sqlItem2);
-		$altaPesaPra = mysqli_fetch_object($queryAltaPesaPra);
-	*/
-		
-		$retorn = $etiqueta;
+		$idPesaNova = mysqli_insert_id($bd);
+		//INSERT REFERENCE TO THE NEW pesa IN obj_any_pra TABLE
+		if ($queryAltaPesa) {
+			// Check if it is the first user's piece for that year
+			$sqlYearExists = "SELECT * FROM obj_any_pra WHERE idPRA = '$user' AND any = '$year'";
+			$queryYearExists = mysqli_query ($bd, $sqlYearExists);
+			$YearExists = mysqli_fetch_object($queryYearExists);
+			$idPosNova = "idPesa" . $position;
+			// If it is, create a new register for the year and insert the piece reference
+			if (mysqli_num_rows($queryYearExists) == 0) {
+				$sqlItem2 = "INSERT INTO obj_any_pra (idPRA, any, ".$idPosNova.") VALUES ('$user', '$year', ".$idPesaNova.");";
+				$queryAltaPesa2 = mysqli_query ($bd, $sqlItem2);
+			}
+			// If not, modify the existing year register to include the piece reference
+			else{
+				$sqlItem2 = "UPDATE obj_any_pra SET ".$idPosNova."= ".$idPesaNova." WHERE idPRA='$user' AND any='$year';";
+				$queryAltaPesa2 = mysqli_query ($bd, $sqlItem2);
+			}						
+		}	
+		$retorn = array("retorn" => $queryAltaPesa2);
 		$response = $response->withJson($retorn, 200);
 		return($response);
 	  	} 
@@ -264,10 +209,7 @@ $app->post('/usuari/altaPeces', function (Request $request, Response $response){
     		$error = $e->getMessage();
   		}
 });
-
-
 $app->post('/usuari/modificaPesa', function (Request $request, Response $response){
-
 	try{
 		$allPostPutParams = $request->getParsedBody();
 		$idPesa = $allPostPutParams['idPesa'];
@@ -279,10 +221,8 @@ $app->post('/usuari/modificaPesa', function (Request $request, Response $respons
 		$validacio = $allPostPutParams['validacio'];
 		$validat = $allPostPutParams['validat'];
 		$comentaris = $allPostPutParams['comentaris'];
-
 		include ("connexio.php"); // Crida al fitxer de la connexió
 		$bd = connectarBD();
-
 		$sqlItemA = "SELECT * FROM pesa WHERE idPesa = '$idPesa'";
 		$queryPesaA = mysqli_query ($bd, $sqlItemA);
 		$PesaA = mysqli_fetch_object($queryPesaA);
@@ -299,7 +239,5 @@ $app->post('/usuari/modificaPesa', function (Request $request, Response $respons
     		$error = $e->getMessage();
   		}
 });
-
 $app->run();
-
 ?>
--- js/historial.js
+++ js/historial.js
@@ -143,14 +143,14 @@ $(document).ready(function(){
 							dataType : 'json',
 							data: {nom:idUsuari,any:anyPesaSubs,posicio:posicioPesaSubs},
 							success : function(result) {
-									  $("#etiqueta").val(result.etiqueta);
-									  $("#tipusP").val(result.tipus);
-									  $("#aplicat").val(result.aplicat);
-									  $("#compromis").val(result.compromis);
-									  $("#acompliment").val(result.acompliment);
-									  //$("#validacio").val(result.validacio);
-									  //$("#validat").val(result.validat);
-									  $("#comments").val(result.comentaris);
+									  var et0 = $("#etiqueta").val(result.etiqueta);
+									  var ti0 = $("#tipusP").val(result.tipus);
+									  var ap0 = $("#aplicat").val(result.aplicat);
+									  var co0 = $("#compromis").val(result.compromis);
+									  var ac0 = $("#acompliment").val(result.acompliment);
+									  //var va0 = $("#validacio").val(result.validacio);
+									  //var vl0 = $("#validat").val(result.validat);
+									  var cm0 = $("#comments").val(result.comentaris);
 									  optionArray = [result.tipusConcat];
 									  var iMax = tipusDePesaTots.length;
 									  var option = '';
@@ -158,58 +158,52 @@ $(document).ready(function(){
   									  	 option += '<option value="' + tipusDePesaTots[i].idTipus + '">' + tipusDePesaTots[i].idTipus + '</option>';
   									    }
 									  $("#tipusP").html(option);
+									  $("#okPesa").click(function(event){ // L'usuari prem l'OK
+									  event.preventDefault();
+									  var et = $("#etiqueta").val();
+									  var ti = $("#tipusP").val();
+									  var ap = $("#aplicat").val();
+									  var co = $("#compromis").val();
+									  var ac = $("#acompliment").val();
+									  //var validacio = $("#validacio").val();
+									  //var validat = $("#validat").val();
+									  var cm = $("#comments").val();
+									  if (optionArray=="") { // Check if the piece is new or modified
+											var missatge = confirm("Confirmes que vols crear una peça nova?"); // Cal aquesta pregunta?
+											if (missatge) { //Create new piece
+															$('#qDialeg').modal('hide');
+															var currURL = window.location.href;
+															var idUsuari = getParameterByName('userId', currURL);
+//															TO DO: Incloure crida AJAX de creació de nova peça
+															$.ajax({
+        															url: 'http://localhost/public/index.php/usuari/altaPesa', // url local
+//																	url: 'http://localhost:8080/public/index.php/usuari/altaPesa', // url local
+								        							type : "POST", // type of action: POST
+        															dataType : 'json', // tipus de dades del retorn
+        															data: {user:idUsuari, any: anyPesaSubs, posicio:posicioPesaSubs, etiqueta:et, tipusP:ti, aplicat:ap, compromis:co, acompliment:ac, comentaris:cm},
+        															success : function(result) {
+	    																				console.log(result);
+																	},
+        															error: function(xhr, resp, text) {
+	    																				console.log(xhr, resp, text);	        																								
+																	}
+															}); //end ajax
+															//Un cop guardada la peça, refrescar pantalla!!!
+															console.log("...peça creada");
+											} else {
+													console.log("No s'ha confirmat la creació de la peça");
+													}
+								 	  } else {
+//											  TO DO: Incloure codi per modificació de peça (primer comprobar si és un canvi menor o major, demnar confirmació, i desprès fer crida AJAX corresponent)
+								 			 } //end modify piece
+									  }) //end okPesa
 							},
 							error: function(xhr, resp, text) {
 		  							console.log(xhr, resp, text)
 		  					}
-  						}) //end ajax
-
-				  	dadesCarregades.done(function(result){
-					// Fem servir una Promise/Deferred per assegurar-nos que no es fa cap altra crida AJAX fins que no s'hagin
-					// carregat les dades de la prèvia
-						$("#okPesa").click(function(){ // Es prem OK
-    					console.log('OK. Seguim');
- 						var etiqueta = $("#etiqueta").val();
-						var tipus = $("#tipusP").val();
-						var aplicat = $("#aplicat").val();
-						var compromis = $("#compromis").val();
-						var acompliment = $("#acompliment").val();
-						var validacio = $("#validacio").val();
-						var validat = $("#validat").val();
-						var comentaris = $("#comments").val();
-						if (optionArray=="") { // Check is the piece is new or modified
-												var missatge = confirm("Vols crear una peça nova?");
-												if (missatge) { //New piece
-																console.log("Comencem a crear peça...");
-																var currURL = window.location.href;
-																var idUsuari = getParameterByName('userId', currURL);
-//																TO DO: Incloure crida AJAX de creació de nova peça
-																/*$.ajax({
-        																url: 'http://localhost/public/index.php/usuari/login', // url local
-//		          														url: 'http://localhost:8080/public/index.php/usuari/login', // url local
-												        				type : "POST", // type of action: POST
-        																dataType : 'json', // tipus de dades del retorn
-        																data: {nom:idUsuari,pass:idUsuari},   // alternativa 2: construeixo jo el json
-        																success : function(result) {
-	        																						console.log(result);
-	        																						document.getElementById('pra_actiu').innerHTML = result.nom + " " + result.cognoms;
-																		},
-        																error: function(xhr, resp, text) {
-	        																							console.log(xhr, resp, text);
-																		}
-																}); //end ajax*/
-																console.log("...peça suposadament creada");
-												}
-												console.log("No s'ha confirmat la creació de la peça");
-											} //end new piece created
-						else {
-//								TO DO: Incloure codi per modificació de peça (primer comprobar si és un canvi menor o major, demnar confirmació, i desprès fer crida AJAX corresponent)
-						} //end modify piece
-						}) //end okPesa
-  					}) //end promise
-  	}); //end piece edition
-
-});  // end $(document).ready(function()
+  						}); //end ajax
+  }) //end piece edition
+})  // end $(document).ready(function()
 
 /* Functions related to years horizontal moves*/
 
